394569cd5e061676ba8db99c6a259199720082bb
Caeli1010 <elizabeth.ayala4@gmail.com>
2021-05-30 19:13:45 -0300

    Inicia proyecto Web

---
+++ WebProductos/Productos.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Productos.aspx.cs" Inherits="WebProductos.Productos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView ID="dvgDetalle" runat="server"></asp:GridView>
+</asp:Content>
---
+++ WebProductos/Productos.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using negocio;
+
+namespace WebProductos
+{
+    public partial class Productos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            dvgDetalle.DataSource = negocio.listar();
+            dvgDetalle.DataBind();
+        }
+    }
+}
